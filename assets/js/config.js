10c98db6656655d967e5f36a895ffccc5b33f3a4
amohamed-alt <a.mohamed@bayt.net>
2026-05-23 23:28:06 +0300

    v3: modular rebuild

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+window.TALENTERA_CONFIG = {
+  supabaseUrl: 'https://czaxtwbmborxwzaboqxl.supabase.co',
+  supabaseKey: 'sb_publishable_uVUdpVWggu1WvkSKCAi51w_9qsb-AjX',
+  hubspotPortal: '145742477',
+  hubspotBase: 'https://app-eu1.hubspot.com/contacts/145742477'
+};

47e875b8b129db059bf621898bb45092495a8853
amohamed-alt <a.mohamed@bayt.net>
2026-05-23 23:53:50 +0300

    v4: premium rebuild with correct schema mapping

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,6 @@
-window.TALENTERA_CONFIG = {
+window.TCC = {
   supabaseUrl: 'https://czaxtwbmborxwzaboqxl.supabase.co',
   supabaseKey: 'sb_publishable_uVUdpVWggu1WvkSKCAi51w_9qsb-AjX',
-  hubspotPortal: '145742477',
-  hubspotBase: 'https://app-eu1.hubspot.com/contacts/145742477'
+  hsPortal: '145742477',
+  hsBase: 'https://app-eu1.hubspot.com/contacts/145742477'
 };

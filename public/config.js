6316a99324f488985035fbc2452d8053a69614c4
amohamed-alt <a.mohamed@bayt.net>
2026-06-02 16:48:55 +0300

    Connect React dashboard to Supabase project

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.TALENTERA_CONFIG = {
   appName: 'Talentera Sales Command Center',
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://czaxtwbmborxwzaboqxl.supabase.co',
+  supabaseAnonKey: 'sb_publishable_uVUdpVWggu1WvkSKCAi51w_9qsb-AjX',
   schema: 'public'
 };
